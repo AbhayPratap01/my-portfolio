a7080c9f74aa5ea13403ce11c2beacd68ebc5a34
AbhayRathore2006 <103251542+AbhayPratap01@users.noreply.github.com>
2025-05-21 10:37:23 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-console.log("Portfolio Loaded Successfully!");
+document.getElementById('themeToggle').addEventListener('click', () => {
+  const html = document.documentElement;
+  const currentTheme = html.getAttribute('data-theme');
+  html.setAttribute('data-theme', currentTheme === 'dark' ? 'light' : 'dark');
+});
